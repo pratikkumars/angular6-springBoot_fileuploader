80d70427c30b25d8f7bd1cd48aa633343c9f611e
pratik <pratikkumars@gmail.com>
2019-05-13 18:12:44 -0700

    adding change

---
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.services.StorageService;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner  {
+
+	@Resource
+	  StorageService storageService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		storageService.deleteAll();
+	    storageService.init();
+		
+	}
+
+}
